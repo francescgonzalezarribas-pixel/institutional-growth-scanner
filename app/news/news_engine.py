a78675a017e9578189ced9bc013097f99f9f7f45
francescgonzalezarribas-pixel <francescgonzalezarribas@gmail.com>
2026-05-09 00:23:59 +0200

    Actualizar news_engine.py

--- app/news/news_engine.py
+++ app/news/news_engine.py
@@ -6,19 +6,38 @@ client = finnhub.Client(api_key=FINNHUB_API_KEY)
 
 
 IMPORTANT_KEYWORDS = [
-    "ai",
-    "artificial intelligence",
     "earnings",
     "guidance",
     "contract",
     "partnership",
+    "artificial intelligence",
+    "ai",
     "semiconductor",
     "satellite",
     "robotics",
+    "cloud",
     "acquisition",
     "defense",
+    "military",
     "growth",
-    "cloud"
+    "expansion",
+    "approval",
+    "deal",
+    "infrastructure"
+]
+
+
+BAD_KEYWORDS = [
+    "top gainers",
+    "top losers",
+    "market recap",
+    "analyst ratings",
+    "price target",
+    "stock moved",
+    "why shares",
+    "why stock",
+    "market close",
+    "trading session"
 ]
 
 
@@ -29,16 +48,29 @@ def get_company_news(symbol: str):
         news = client.company_news(
             symbol,
             _from="2026-05-01",
-            to="2026-05-08"
+            to="2026-05-09"
         )
 
         filtered = []
 
-        for item in news[:10]:
+        for item in news[:15]:
 
-            headline = item.get("headline", "").lower()
+            headline = item.get(
+                "headline",
+                ""
+            ).lower()
 
-            if any(k in headline for k in IMPORTANT_KEYWORDS):
+            if any(
+                bad in headline
+                for bad in BAD_KEYWORDS
+            ):
+                continue
+
+            if any(
+                good in headline
+                for good in IMPORTANT_KEYWORDS
+            ):
+
                 filtered.append(item)
 
         return filtered
