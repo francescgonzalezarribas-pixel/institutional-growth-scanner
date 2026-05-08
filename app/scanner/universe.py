79eda2aac5173367ac14f5819cf1ca029c95975e
francescgonzalezarribas-pixel <francescgonzalezarribas@gmail.com>
2026-05-09 00:28:48 +0200

    Crear app/scanner/universe.py

---
+++ app/scanner/universe.py
@@ -0,0 +1,37 @@
+HOT_STOCKS = [
+
+    # IA
+    "NVDA",
+    "PLTR",
+    "SMCI",
+    "ARM",
+    "SOUN",
+    "BBAI",
+
+    # Space / Defense
+    "RKLB",
+    "ASTS",
+    "LUNR",
+
+    # Quantum / Robotics
+    "IONQ",
+    "SYM",
+
+    # Cybersecurity
+    "CRWD",
+    "PANW",
+
+    # Growth
+    "HIMS",
+    "TEM",
+    "CAVA",
+
+    # Semiconductors
+    "AMD",
+    "TSM",
+    "MU",
+
+    # IPO / Newer
+    "RDDT",
+    "CART"
+]
