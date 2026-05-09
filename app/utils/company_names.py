e90c3e56b65ebaefa63219f67816541978d56de8
francescgonzalezarribas-pixel <francescgonzalezarribas@gmail.com>
2026-05-09 13:26:06 +0200

    Crear app/utils/company_names.py

---
+++ app/utils/company_names.py
@@ -0,0 +1,21 @@
+COMPANY_NAMES = {
+
+    "RKLB": "Rocket Lab",
+    "ASTS": "AST SpaceMobile",
+    "PLTR": "Palantir",
+    "NVDA": "NVIDIA",
+    "ARM": "ARM Holdings",
+    "IONQ": "IonQ",
+    "HIMS": "Hims & Hers",
+    "TEM": "Tempus AI",
+    "CRWD": "CrowdStrike",
+    "RDDT": "Reddit",
+    "CART": "Instacart",
+    "AMD": "AMD",
+    "SMCI": "Super Micro Computer",
+    "SOUN": "SoundHound AI",
+    "ASML": "ASML",
+    "STM": "STMicroelectronics",
+    "RHM.DE": "Rheinmetall",
+    "DARK.L": "Darktrace"
+}
