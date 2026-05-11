49574c9ea5f483cc2a415707fa5dd81c5b7def8a
francescgonzalezarribas-pixel <francescgonzalezarribas@gmail.com>
2026-05-11 12:51:37 +0200

    Actualizar main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 from app.scanner.market_scanner import scan_market
 from app.telegram.telegram_bot import send_message
@@ -10,12 +11,21 @@ from app.utils.market_hours import (
 )
 
 
-sent = set()
+# =========================
+# ANTI DUPLICADOS
+# =========================
+
+sent_signals = {}
+
+# 4 horas
+SIGNAL_COOLDOWN = 14400
 
 
 async def process_signals():
 
     signals = scan_market()
+
+    now = time.time()
 
     for signal in signals:
 
@@ -24,17 +34,27 @@ async def process_signals():
             f"-{signal['headline']}"
         )
 
-        if key in sent:
-            continue
+        # ya enviada recientemente
+        if key in sent_signals:
+
+            last_sent = sent_signals[key]
+
+            if (
+                now - last_sent
+                < SIGNAL_COOLDOWN
+            ):
+
+                continue
 
         msg = build_signal(signal)
 
         await send_message(msg)
 
-        sent.add(key)
+        sent_signals[key] = now
 
         log.info(
-            f"Señal enviada: {signal['symbol']}"
+            f"Señal enviada: "
+            f"{signal['symbol']}"
         )
 
 
@@ -50,27 +70,25 @@ async def main():
 
             if market_is_open():
 
-                log.info(
-                    "Mercado abierto → escaneando"
-                )
-
                 await process_signals()
 
-                await asyncio.sleep(900)
+                # 20 min
+                await asyncio.sleep(1200)
 
             else:
 
                 log.info(
-                    "Mercado cerrado"
+                    "Mercados cerrados"
                 )
 
+                # revisar cada 5 min
                 await asyncio.sleep(300)
 
         except Exception as e:
 
             log.error(e)
 
-            await asyncio.sleep(300)
+            await asyncio.sleep(60)
 
 
 if __name__ == "__main__":
